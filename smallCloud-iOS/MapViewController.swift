4e6b8d2dc84f45a85e580ff97680d2f2031e7ae6
10000Kim <kckm317@gmail.com>
2023-06-01 18:10:35 +0900

    지도에 현재 위치 표시와 주변 15개 동물병원 표시 cocoapods을 이용해 RESTAPI사용을 위해 Alamofire, SwiftyJSON 설치 후 작업했습니다.

--- smallCloud-iOS/MapViewController.swift
+++ smallCloud-iOS/MapViewController.swift
@@ -8,6 +8,20 @@
 import UIKit
 import CoreLocation
 
+//카카오RESTAPI를 사용하기위해
+import Alamofire
+import SwiftyJSON
+
+public struct Place{
+        let placeName :String
+        let roadAdressName:String
+        let longitudeX:String
+        let latitudeY:String
+    }
+    
+var resultList=[Place]()
+
+
 class MapViewController: UIViewController, MTMapViewDelegate {
 
     var mapView:MTMapView!
@@ -50,8 +64,6 @@ class MapViewController: UIViewController, MTMapViewDelegate {
         })
         
         
-        
-
         mapView = MTMapView(frame: CGRect(x: 0, y: 50, width: 450, height: 780))
         //mapView = MTMapView(frame: self.view.frame)
         mapView.delegate = self
@@ -60,36 +72,147 @@ class MapViewController: UIViewController, MTMapViewDelegate {
         mapView.setMapCenter(myMapCenter, animated: true)
         self.view.addSubview(mapView)
         
-
+        // 현재 위치 트래킹
+        mapView.currentLocationTrackingMode = .onWithoutHeading
+        mapView.showCurrentLocationMarker = true
+        
+        //createPin2(itemNames: "현위치", getla: la, getlo: lo, customImageName: "user-gps-marker")
+        
+        addAnimalHospitalMarkers()
         // Do any additional setup after loading the view.
     }
     
+    
+    //func createPin(itemNames: String, getla:Double, getlo:Double, markerType: MTMapPOIItemMarkerType) -> MTMapPOIItem {
+    //    let poiItem = MTMapPOIItem()
+    //    poiItem.itemName = "\(itemNames)"
+    //    poiItem.mapPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude: getla, longitude: getlo))
+    //
+    //    poiItem.markerType = markerType
+    //
+    //    mapView.addPOIItems([poiItem])
+    //    return poiItem
+    //}
+
+
+    func createPin2(itemNames: String, getla: Double, getlo: Double, customImageName: String) -> MTMapPOIItem {
+        let poiItem = MTMapPOIItem()
+        poiItem.itemName = itemNames
+        poiItem.mapPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude: getla, longitude: getlo))
+        
+        poiItem.markerType = .customImage
+        poiItem.customImageName = customImageName
+        
+        mapView.addPOIItems([poiItem])
+        return poiItem
+    }
+
+    
+    
 }
 
+
 extension MapViewController:CLLocationManagerDelegate{
-    func getLocationUsagePermission(){
-        self.locationManager.requestWhenInUseAuthorization()
-    }
+//    func getLocationUsagePermission(){
+//        self.locationManager.requestWhenInUseAuthorization()
+//    }
     
-    func locationManager(_ manager:CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus){
-        switch status {
-        case.authorizedAlways, .authorizedWhenInUse:
-            print("GPS 권한 설정")
-        case .restricted, .notDetermined:
-            print("GPS 권한 설정되지 않음")
-            getLocationUsagePermission()
-        case .denied:
-            print("GPS 권한 요청 거부")
-            getLocationUsagePermission()
-        default:
-            print("GPS: Default")
+    func getLocationUsagePermission() {
+            DispatchQueue.main.async {
+            self.locationManager.requestWhenInUseAuthorization()
+            }
         }
-    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+            switch manager.authorizationStatus {
+            case .authorizedAlways, .authorizedWhenInUse:
+                print("GPS 권한 설정")
+                // 필요한 동작 수행
+                locationManager.startUpdatingLocation()
+            case .restricted, .notDetermined:
+                print("GPS 권한 설정되지 않음")
+                getLocationUsagePermission()
+            case .denied:
+                print("GPS 권한 요청 거부")
+                getLocationUsagePermission()
+            default:
+                print("GPS: Default")
+            }
+        }
+    
+    
+//    func locationManager(_ manager:CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus){
+//        switch status {
+//        case.authorizedAlways, .authorizedWhenInUse:
+//            print("GPS 권한 설정")
+//        case .restricted, .notDetermined:
+//            print("GPS 권한 설정되지 않음")
+//            getLocationUsagePermission()
+//        case .denied:
+//            print("GPS 권한 요청 거부")
+//            getLocationUsagePermission()
+//        default:
+//            print("GPS: Default")
+//        }
+//    }
     func locationManager(_ manager:CLLocationManager, didUpdateLocations locations:[CLLocation]){
         let location: CLLocation = locations[locations.count-1]
         let longtitude: CLLocationDegrees = location.coordinate.longitude
         let latitude: CLLocationDegrees = location.coordinate.latitude
     }
+    
+    //카카오RESTAPI
+    func addAnimalHospitalMarkers() {
+        guard let currentLocation = locationManager.location?.coordinate else {
+                return
+            }
+           let animalHospitalAPIURL = "https://dapi.kakao.com/v2/local/search/keyword.json"
+        let headers: HTTPHeaders = [
+                "Authorization": "KakaoAK 07897825ca4d62b2f12ae645b244d091"
+            ]
+        let parameters: Parameters = [
+                    "query": "동물병원",
+                    "x": currentLocation.longitude,
+                    "y": currentLocation.latitude
+                ]
+        
+        
+        AF.request(animalHospitalAPIURL, parameters: parameters, headers: headers).responseJSON { response in
+            print(response) // 응답 출력
+                    switch response.result {
+                    case .success(let value):
+                        let json = JSON(value)
+                        let documents = json["documents"].arrayValue
+
+                        for document in documents {
+                            if let placeName = document["place_name"].string,
+                               let roadAddressName = document["road_address_name"].string,
+                               let longitudeX = document["x"].string,
+                                let latitudeY = document["y"].string
+                                {
+                                let place = Place(placeName: placeName, roadAdressName: roadAddressName, longitudeX: longitudeX, latitudeY: latitudeY)
+                                resultList.append(place)
+
+                                self.addMarkerToKakaoMap(place: place)
+                            }
+                        }
+
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+            }
+            func addMarkerToKakaoMap(place: Place) {
+            let marker = MTMapPOIItem()
+            marker.itemName = place.placeName
+            marker.mapPoint = MTMapPoint(geoCoord: MTMapPointGeo(latitude: Double(place.latitudeY) ?? 0.0, longitude: Double(place.longitudeX) ?? 0.0))
+            
+            marker.markerType = .customImage
+            marker.customImageName = "animal-hospital-marker"
+
+            mapView.addPOIItems([marker])
+                print("Added marker: \(place.placeName)") // 마커 추가 로그 출력
+        }
     
 }
 
